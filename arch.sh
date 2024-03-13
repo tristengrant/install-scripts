fbe315dd98a51b058df8bce01c0a7645003c8a6d
Tristen Grant <hello@tristengrant.com>
2024-03-12 19:36:14 -0500

    added first verion of arch install bash script

---
+++ arch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#########################
+
+sudo pacman -S pcmanfm krita obsidian flatpak python-dbus-next obs-studio github-cli git crcklib arch-audit zsh vim neovim gimp inkscape kdenlive volumeicon rofi network-manager-applet qemu-desktop virt-manager iptables-nft emacs ripgrep unzip fd xfce4-screenshooter btop neofetch htop dunst khal bat discord steam xclip xorg-xclipboard chromium ristretto picom lf kvantum kvantum-qt5 fish rkhunter alacritty go ttf-roboto-mono strawberry

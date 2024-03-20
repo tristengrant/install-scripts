b4bc406d0d59c92df62c688d6a2bda5faea896ff
Tristen Grant <hello@tristengrant.com>
2024-03-20 01:44:24 -0500

    Updates

--- arch.sh
+++ arch.sh
@@ -2,4 +2,4 @@
 
 ## Installs various applications ##
 
-sudo pacman -S picom sxhkd pcmanfm krita obsidian flatpak python-dbus-next github-cli nwg-look wget vim neovim gimp inkscape volumeicon network-manager-applet qemu-desktop virt-manager emacs ripgrep unzip fd neofetch htop dunst discord steam kvantum kvantum-qt5 alacritty kitty go ttf-roboto-mono strawberry rhythmbox lollypop quodlibet noto-fonts-emoji ttf-ubuntu-font-family bitwarden ranger ventoy timeshift
+sudo pacman -S picom sxhkd pcmanfm krita obsidian flatpak python-dbus-next github-cli nwg-look wget vim neovim gimp inkscape volumeicon network-manager-applet qemu-desktop virt-manager emacs ripgrep unzip fd neofetch htop dunst discord steam kvantum kvantum-qt5 alacritty kitty go ttf-roboto-mono strawberry rhythmbox lollypop quodlibet noto-fonts-emoji ttf-ubuntu-font-family bitwarden ranger ventoy timeshift bspwm dmenu wofi xmobar xmonad-contrib awesome
